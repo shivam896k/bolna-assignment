3ad45156afb2b126afe687bc618d1c78e7373c3a
shivam singh <shivamkumarsingh10000@gmail.com>
2025-11-06 23:56:18 +0530

    Fix timedelta argument in make_request method

--- api_client/open_ai_api_client.py
+++ api_client/open_ai_api_client.py
@@ -11,7 +11,7 @@ class OpenAiApiClient:
     def make_request(self, params):
         try:
             # params['start_at'] = '2025-07-06T04:10:34.846Z'
-            params['start_at'] = (datetime.now() - timedelta(minutes==10)).strftime("%Y-%m-%dT%H:%M:%SZ")
+            params['start_at'] = (datetime.now() - timedelta(minutes=10)).strftime("%Y-%m-%dT%H:%M:%SZ")
             params['end_at'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
             response = requests.get(self.base_url, params=params, timeout=5)
             data = response.json()
